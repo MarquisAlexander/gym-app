b06e735d95cbe554a7e568033fec9f74158b96d2
marquisalexander <marquis.alexander@outlook.com>
2023-01-18 20:12:30 -0300

    feat: add native base lib

--- App.tsx
+++ App.tsx
@@ -1,30 +1,28 @@
-import { Text, View, StatusBar } from "react-native";
+import { View, StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
 import {
 	useFonts,
 	Roboto_400Regular,
 	Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
+import { THEME } from "./src/theme";
+import { Loading } from "@components/Loading";
+
 export default function App() {
 	const [fontsLoaded] = useFonts({
 		Roboto_400Regular,
 		Roboto_700Bold,
 	});
+
 	return (
-		<View
-			style={{
-				flex: 1,
-				alignItems: "center",
-				justifyContent: "center",
-				backgroundColor: "#202020",
-			}}
-		>
+		<NativeBaseProvider theme={THEME}>
 			<StatusBar
 				barStyle="light-content"
 				backgroundColor="transparent"
 				translucent
 			/>
-			{fontsLoaded ? <Text>Hello world!</Text> : <View />}
-		</View>
+			{!fontsLoaded ? <View /> : <Loading />}
+		</NativeBaseProvider>
 	);
 }
