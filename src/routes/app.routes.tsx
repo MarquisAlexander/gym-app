9c1535c34b212fdf57d0ebcdce865504163280a8
marquisalexander <marquis.alexander@outlook.com>
2023-01-23 20:31:32 -0300

    feat: add bottom tab navigation

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,67 @@
+import {
+	createBottomTabNavigator,
+	BottomTabNavigationProp,
+} from "@react-navigation/bottom-tabs";
+import HistorySvg from "@assets/history.svg";
+import HomeSvg from "@assets/home.svg";
+import ProfileSvg from "@assets/profile.svg";
+
+import { Home } from "@screens/Home";
+import { Exercise } from "@screens/Exercise";
+import { Profile } from "@screens/Profile";
+import { History } from "@screens/History";
+import { useTheme } from "native-base";
+
+type AppRoutes = {
+	home: undefined;
+	exercise: undefined;
+	history: undefined;
+	profile: undefined;
+};
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+
+export function AppRoutes() {
+	const { sizes } = useTheme();
+
+	const iconSize = sizes[6];
+	return (
+		<Navigator
+			screenOptions={{
+				headerShown: false,
+				tabBarShowLabel: false,
+			}}
+		>
+			<Screen
+				name="home"
+				component={Home}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<HomeSvg fill={color} width={iconSize} height={iconSize} />
+					),
+				}}
+			/>
+			<Screen
+				name="history"
+				component={History}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<HistorySvg fill={color} width={iconSize} height={iconSize} />
+					),
+				}}
+			/>
+			<Screen
+				name="profile"
+				component={Profile}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<ProfileSvg fill={color} width={iconSize} height={iconSize} />
+					),
+				}}
+			/>
+			<Screen name="exercise" component={Exercise} />
+		</Navigator>
+	);
+}
