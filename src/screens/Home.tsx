df64bc18f1d83a8660a41c34ab42cc493853d240
marquisalexander <marquis.alexander@outlook.com>
2023-01-28 01:32:28 -0300

    feat: create list at exercises

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,6 +7,12 @@ import { ExerciseCard } from "@components/ExerciseCard";
 
 export function Home() {
 	const [groups, setGroups] = useState(["costa", "ombro", "bíceps", "Tríceps"]);
+	const [exercises, setExercises] = useState([
+		"Puxada frontal",
+		"Remada curvada",
+		"Remada unilateral",
+		"Levantamento terra",
+	]);
 	const [groupSelected, setGroupSelected] = useState("costa");
 
 	return (
@@ -37,12 +43,17 @@ export function Home() {
 					</Heading>
 
 					<Text color="gray.200" fontSize="sm">
-						4
+						{exercises.length}
 					</Text>
 				</HStack>
 
-				<ExerciseCard />
-				<ExerciseCard />
+				<FlatList
+					data={exercises}
+					keyExtractor={(item) => item}
+					renderItem={({ item }) => <ExerciseCard />}
+					showsVerticalScrollIndicator={false}
+					_contentContainerStyle={{ paddingBottom: 20 }}
+				/>
 			</VStack>
 		</VStack>
 	);
