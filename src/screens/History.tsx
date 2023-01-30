9b8407344c7b33fe25932e79810f25b2cc8332cb
marquisalexander <marquis.alexander@outlook.com>
2023-01-29 22:23:00 -0300

    feat: add history screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,28 @@
-import { Center, Text } from "native-base";
+import { Heading, VStack, SectionList } from "native-base";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryCard } from "@components/HistoryCard";
+import { useState } from "react";
 
 export function History() {
+	const [exercises, setExercises] = useState([
+		{ title: "26.08.22", data: ["Puxada frontal", "Remada unilateral"] },
+		{ title: "28.08.22", data: ["Puxada frontal"] },
+	]);
 	return (
-		<Center flex={1}>
-			<Text color="white">History</Text>
-		</Center>
+		<VStack flex={1}>
+			<ScreenHeader title="Histórico de Exercícios" />
+			<SectionList
+				sections={exercises}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => <HistoryCard />}
+				renderSectionHeader={({ section }) => (
+					<Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+						{section.title}
+					</Heading>
+				)}
+				px={8}
+			/>
+		</VStack>
 	);
 }
