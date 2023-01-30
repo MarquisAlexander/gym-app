50086b0c3052da1752305ec813ed21d1c6a51ab9
marquisalexander <marquis.alexander@outlook.com>
2023-01-30 19:31:01 -0300

    feat: create profile screens

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,75 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+import {
+	Center,
+	ScrollView,
+	VStack,
+	Skeleton,
+	Text,
+	Heading,
+} from "native-base";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+	const [photoIsLoading, setPhotoIsLoading] = useState(true);
+
 	return (
-		<Center flex={1}>
-			<Text color="white">Profile</Text>
-		</Center>
+		<VStack flex={1}>
+			<ScreenHeader title="Perfil" />
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<Center mt={6} px={10}>
+					{photoIsLoading ? (
+						<Skeleton
+							w={PHOTO_SIZE}
+							h={PHOTO_SIZE}
+							rounded="full"
+							startColor="gray.500"
+							endColor="gray.400"
+						/>
+					) : (
+						<UserPhoto
+							source={{ uri: "https://github.com/marquisalexander.png" }}
+							alt="Foto do usuário"
+							size={PHOTO_SIZE}
+						/>
+					)}
+					<TouchableOpacity>
+						<Text
+							color="green.500"
+							fontWeight="bold"
+							fontSize="md"
+							mt={2}
+							mb={8}
+						>
+							Alterar foto
+						</Text>
+					</TouchableOpacity>
+					<Input placeholder="Nome" bg="gray.600" />
+					<Input placeholder="E-mail" bg="gray.600" isDisabled />
+				</Center>
+
+				<VStack px={10} mt={12} mb={9}>
+					<Heading color="gray.200" fontSize="md" mb={2}>
+						Alterar senha
+					</Heading>
+
+					<Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+					<Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+					<Input
+						bg="gray.600"
+						placeholder="Confirme a nova senha"
+						secureTextEntry
+					/>
+
+					<Button title="Atualizar" mt={4} />
+				</VStack>
+			</ScrollView>
+		</VStack>
 	);
 }
