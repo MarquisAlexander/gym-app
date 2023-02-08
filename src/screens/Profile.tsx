85b8a669b2f288a8371f3ab1574c39bed2a665f9
marquisalexander <marquis.alexander@outlook.com>
2023-02-08 18:49:31 -0300

    feat: add limit size to upload image

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 import {
 	Center,
 	ScrollView,
@@ -7,8 +7,10 @@ import {
 	Skeleton,
 	Text,
 	Heading,
+	useToast,
 } from "native-base";
 import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
@@ -23,19 +25,42 @@ export function Profile() {
 		"https://github.com/marquisalexander.png",
 	);
 
+	const toast = useToast();
+
 	async function handleUserPhotoSelect() {
-		const photoSelected = await ImagePicker.launchImageLibraryAsync({
-			mediaTypes: ImagePicker.MediaTypeOptions.Images,
-			quality: 1,
-			aspect: [4, 4],
-			allowsEditing: true,
-		});
+		setPhotoIsLoading(true);
 
-		console.log(photoSelected);
+		try {
+			const photoSelected = await ImagePicker.launchImageLibraryAsync({
+				mediaTypes: ImagePicker.MediaTypeOptions.Images,
+				quality: 1,
+				aspect: [4, 4],
+				allowsEditing: true,
+			});
 
-		if (photoSelected.canceled) return;
+			console.log(photoSelected);
 
-		setUserPhoto(photoSelected.assets[0].uri);
+			if (photoSelected.canceled) return;
+
+			if (photoSelected.assets[0].uri) {
+				const photoInfo = await FileSystem.getInfoAsync(
+					photoSelected.assets[0].uri,
+				);
+
+				if (photoInfo.size && photoInfo.size / 1024 / 1024 > 2) {
+					return toast.show({
+						title: "Essa imagem é muito grande. Escolha uma de até 5MB.",
+						placement: "top",
+						bgColor: 'red.500'
+					});
+				}
+				setUserPhoto(photoSelected.assets[0].uri);
+			}
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setPhotoIsLoading(false);
+		}
 	}
 
 	return (
