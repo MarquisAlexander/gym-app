e0b1b1335d5ccbd0b8f4eeb48cfec183abc216a3
marquisalexander <marquis.alexander@outlook.com>
2023-02-10 23:47:50 -0300

    feat: add validation to password input

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -17,8 +17,12 @@ type FormDataProps = {
 };
 
 const signUpSchema = yup.object({
-	name: yup.string().required("Informe o nome"),
-	email: yup.string().required("Informe o e-mail").email("E-mail inválido"),
+	name: yup.string().required("Informe o nome."),
+	email: yup.string().required("Informe o e-mail.").email("E-mail inválido."),
+	password: yup
+		.string()
+		.required("Informe a senha.")
+		.min(6, "A senha deve ter pelo menos 6 dígitos."),
 });
 
 export function SignUp() {
@@ -110,6 +114,7 @@ export function SignUp() {
 							secureTextEntry
 							onChangeText={onChange}
 							value={value}
+							errorMessage={errors.password?.message}
 						/>
 					)}
 				/>
