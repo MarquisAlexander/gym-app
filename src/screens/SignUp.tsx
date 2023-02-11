cb2af34e88343d7a52e5a74e253ed6a8f404bea8
marquisalexander <marquis.alexander@outlook.com>
2023-02-10 23:36:25 -0300

    feat: add styles to invalid inputs

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -71,11 +71,14 @@ export function SignUp() {
 						required: "Informe o nome",
 					}}
 					render={({ field: { onChange, value } }) => (
-						<Input placeholder="Nome" onChangeText={onChange} value={value} />
+						<Input
+							placeholder="Nome"
+							onChangeText={onChange}
+							value={value}
+							errorMessage={errors.name?.message}
+						/>
 					)}
 				/>
-
-				<Text color="white">{errors.name?.message}</Text>
 
 				<Controller
 					control={control}
@@ -94,11 +97,10 @@ export function SignUp() {
 							autoCapitalize="none"
 							onChangeText={onChange}
 							value={value}
+							errorMessage={errors.email?.message}
 						/>
 					)}
 				/>
-
-				<Text color="white">{errors.email?.message}</Text>
 
 				<Controller
 					control={control}
