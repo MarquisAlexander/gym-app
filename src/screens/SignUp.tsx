4a516003d75c0cc3bb85cfbfd1d3154148c434a8
marquisalexander <marquis.alexander@outlook.com>
2023-02-24 21:26:13 -0300

    feat: added axios

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,10 +4,14 @@ import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
+import axios from "axios";
+import { api } from "@services/api";
+
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { Alert } from "react-native";
 
 type FormDataProps = {
 	name: string;
@@ -44,23 +48,20 @@ export function SignUp() {
 		navigation.goBack();
 	}
 
-	function handleSignUp({
+	async function handleSignUp({
 		name,
 		email,
 		password,
 		password_confirm,
 	}: FormDataProps) {
-		console.log(JSON.stringify({ name, email, password }));
-		fetch("http://10.0.0.103:3333/users", {
-			method: "POST",
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ name, email, password }),
-		})
-			.then((response) => response.json())
-			.then((data) => console.log(data));
+		try {
+			const response = await api.post("/users", { name, email, password });
+			console.log(response.data);
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				Alert.alert(error.response?.data?.message);
+			}
+		}
 	}
 
 	return (
