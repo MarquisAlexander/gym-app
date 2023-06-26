9e6f7af5924156f70cf491f93b0f2523fbefe7f9
C1331926 Marquis Alexander Santos <C1331926@interno.bb.com.br>
2023-06-26 17:20:29 -0300

    feature: install async storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, createContext, useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
-
 import { api } from "@services/api";
+import { storageUserSave } from "@storage/storageUser";
 
 export type AuthContextDataProps = {
 	user: UserDTO;
@@ -25,6 +25,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
 			if (data.user) {
 				setUser(data.user);
+				storageUserSave(data.user);
 			}
 		} catch (error) {
 			throw error;
