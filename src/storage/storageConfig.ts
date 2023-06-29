bb780734a137f5945c99e011b2a824203555ef2c
C1331926 Marquis Alexander Santos <C1331926@interno.bb.com.br>
2023-06-29 14:20:45 -0300

    feature: save user token

--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
-const USER_STORAGE = '@gymignite:user';
+const USER_STORAGE = '@gymignitegym:user';
+const AUTH_TOKEN_STORAGE = '@gymignitegym:token'
 
-export { USER_STORAGE };
+export { USER_STORAGE, AUTH_TOKEN_STORAGE };
